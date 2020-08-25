b67ee581d287bbdedf0f4b582609cb67e8ee5d48
arthurpicht <info@arthurpicht.de>
2020-08-25 15:09:16 +0200

    init 0.0.8-SNAPSHOT

--- src/main/java/org/mentalizr/mdpCompiler/Const.java
+++ src/main/java/org/mentalizr/mdpCompiler/Const.java
@@ -2,7 +2,13 @@ package org.mentalizr.mdpCompiler;
 
 public class Const {
 
-    public static final String VERSION = "0.0.7";
+    // Set verstion string for release at:
+    // * here
+    // * build.gradle
+    // * mdpc script
+    // * mdpc.cmd script
+
+    public static final String VERSION = "0.0.8-SNAPSHOT";
 
     public static final String VERSION_DATE = "25.08.2020";
 
