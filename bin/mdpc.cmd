06ff72724fe0b93311923b64c4a9d795eb4a01e1
Arthur Picht <picht@arthurpicht.de>
2021-02-05 16:51:45 +0100

    prepare release 0.0.8

--- bin/mdpc.cmd
+++ bin/mdpc.cmd
@@ -1,3 +1,3 @@
 @echo %off
 
-java -cp "%~dp0/../build/libs/mdp-compiler-0.0.8-SNAPSHOT.jar;%~dp0/libs/*" org.mentalizr.mdpCompiler.MDPCompilerCLI %*
+java -cp "%~dp0/../build/libs/mdp-compiler-0.0.8.jar;%~dp0/libs/*" org.mentalizr.mdpCompiler.MDPCompilerCLI %*
