f42a3972b1933e82cab7d9eba07c76a07d465a26
Arthur Picht <picht@arthurpicht.de>
2021-02-05 17:00:02 +0100

    switched to 0.0.9-snapshot

--- src/main/java/org/mentalizr/mdpCompiler/Const.java
+++ src/main/java/org/mentalizr/mdpCompiler/Const.java
@@ -8,7 +8,7 @@ public class Const {
     // * mdpc script
     // * mdpc.cmd script
 
-    public static final String VERSION = "0.0.8-SNAPSHOT";
+    public static final String VERSION = "0.0.9-SNAPSHOT";
 
     public static final String VERSION_DATE = "05.02.2021";
 
