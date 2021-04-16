2f3ffa451bbc0c7a5d37ab58bca3ca551e943cf2
Arthur Picht <picht@arthurpicht.de>
2021-04-16 17:33:43 +0200

    MDPSyntaxError: extendes message, clean-ups

--- src/main/java/org/mentalizr/mdpCompiler/MDPSyntaxError.java
+++ src/main/java/org/mentalizr/mdpCompiler/MDPSyntaxError.java
@@ -2,6 +2,8 @@ package org.mentalizr.mdpCompiler;
 
 import org.mentalizr.mdpCompiler.document.Line;
 
+import java.nio.file.Path;
+
 public class MDPSyntaxError extends Exception {
 
     protected Line line;
@@ -24,4 +26,13 @@ public class MDPSyntaxError extends Exception {
     public Line getLine() {
         return this.line;
     }
+
+    public static String getExtendedMessage(Path path, MDPSyntaxError mdpSyntaxError) {
+        return "Syntax error in [" + path.toAbsolutePath() + ":" + mdpSyntaxError.getLine().getLineNr() + "] " + mdpSyntaxError.getMessage();
+    }
+
+    public static String getExtendedMessage(String id, MDPSyntaxError mdpSyntaxError) {
+        return "Syntax error in [" + id + ":" + mdpSyntaxError.getLine().getLineNr() + "] " + mdpSyntaxError.getMessage();
+    }
+
 }
